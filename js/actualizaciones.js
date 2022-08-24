f6b502595ac655ebc0e4df3a99d5634208d4558b
LuisAngelMaciel <69272932+hiLuisXiib@users.noreply.github.com>
2022-08-24 16:52:41 -0500

    Add files via upload

---
+++ js/actualizaciones.js
@@ -0,0 +1,38 @@
+var myObject = {
+    "publicacionesXiiber": [{
+        "fechaDePublicacion": "14-agosto-2020",
+        "Post": "Hice tal cosa",
+        "enlace": "https://www.xiiber.com/index.html",
+        "href": "https://www.xiiber.com/index.html",
+        "data-href": "index",
+        "img-avatar": "https://www.xiiber.com/favicon.png",
+        "alt-avatar": "avatar",
+        "alt-avatar": "avatar",
+        "pageid": "viajes",
+        "accion": "Descargar"
+
+
+    }, {
+        "fechaDePublicacion": "15-agosto-2020",
+        "Post": "Luego o tra cosa Luego o tra cosa Luego o tra cosa Luego o tra cosa Luego o tra cosa Luego o tra cosa ",
+        "enlace": "https://www.xiiber.com",
+        "href: ": "https: //tepozspa.xiiber.com",
+        "data-href": "xiiber",
+        "img-avatar": "https://www.xiiber.com/favicon.png",
+        "alt-avatar": "avatar",
+        "pageid": "viajes",
+        "accion": "leer"
+
+    }, {
+        "fechaDePublicacion": "16-agosto-2020",
+        "Post": "Y acabe",
+        "enlace": "leer ahora",
+        "href: ": "www.xiiber.com",
+        "img-avatar": "https://www.xiiber.com/favicon.png",
+        "alt-avatar": "avatar",
+        "pageid": "viajes",
+        "data-href": "mapadesitio",
+        "id": "mapadesitio",
+        "accion": "ir"
+    }]
+};
